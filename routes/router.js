0f5258cae88fbf0b785ad3ca42b61f82f3b312be
Evan Johnson <78399565+Technocrazy8@users.noreply.github.com>
2022-12-12 13:24:18 -0800

    test of router

--- routes/router.js
+++ routes/router.js
@@ -3,7 +3,7 @@ const router = createBrowserRouter([
 
         element: <Index />,
 
-        path: "technocrazy8.github.io",
+        path: "technocrazy8.github.io/index.html",
 
         loader: async ({request, params}) =>{
             return fetch(
